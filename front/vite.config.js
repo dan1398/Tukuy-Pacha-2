3c58209205c0dd42b85b491718f11258162eb1a1
danny <d1303gm@gmail.com>
2025-11-05 16:09:22 -0400

    front

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,5 +1,4 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
@@ -15,4 +14,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  optimizeDeps: {
+    exclude: ['vite-plugin-vue-devtools'] // 🔹 Evita el error ENOENT
+  }
 })
